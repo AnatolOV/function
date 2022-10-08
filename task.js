256d0d4773a99cb71e17314cb1fc7e9f3a8d6e22
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2022-10-08 14:37:49 +0300

    task1

--- task.js
+++ task.js
@@ -1,17 +1,35 @@
 // Задание 1
+var arr = [-100, 20, 30, 55, 100];
+//let min = 1, max = 4;
 function getArrayParams(arr) {
-  fffg
-dffg
-ert
 
-  let min, max, sum, avg;
 
-  // Ваш код
+  let min = -100, max = 100, sum = 0, avg = 20;
 
-  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) {
+      max = arr[i];
+
+    }
+    sum += arr[i]
+    avg = (sum / i).toFixed(2);
+    avg = Number(avg);
+  }
+
+  console.log(max, sum, avg);
+
+  for (let j = arr.length; j > -1; j--) {
+    if (arr[j] < arr[j + 1]) {
+      min = arr[j];
+    }
+  }
+
+  console.log({ min: min, max: max, avg: avg })
+
 
   return { min: min, max: max, avg: avg };
 }
+getArrayParams(arr);
 
 // Задание 2
 function worker(arr) {
